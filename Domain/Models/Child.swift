df0b4075e74ead720fa0586e70edffcf14892e88
Andrew Chersky <andrew.chersky@gmail.com>
2020-11-16 07:48:18 +0200

    Update Child model in Domain.framework.

--- Domain/Models/Child.swift
+++ Domain/Models/Child.swift
@@ -9,10 +9,15 @@ import Foundation
 
 public struct Child: Codable, Hashable {
     
-    public let name: String
     public let title: String
-    public let url: URL
     public let author: String
-    public let numComments: Int?
+    public let numComments: Int
+    public let createdUtc: Date
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, author
+        case createdUtc = "created_utc"
+        case numComments = "num_comments"
+    }
     
 }
